a23a2df9292e1bbe8540471985eef06e5986b8a7
Filipe Alves <filipe.alvesdsouza@gmail.com>
2022-03-08 16:02:34 -0300

    Add tools infos

---
+++ src/static/tools.js
@@ -0,0 +1,54 @@
+const tools = [
+  {
+    name: 'Discord',
+    description: 'Aplicativo de voz sobre IP proprietário e gratuito, projetado inicialmente para comunidades de jogos',
+    image: require('../assets/images/discord.jpg')
+  },
+  {
+    name: 'Google Drive',
+    description: 'Serviço de armazenamento e sincronização de arquivos',
+    image: require('../assets/images/drive.jpg')
+  },
+  {
+    name: 'Figma',
+    description: 'Editor gráfico de vetor e prototipagem de projetos de design',
+    image: require('../assets/images/figma.jpg')
+  },
+  {
+    name: 'Git',
+    description: 'Sistema de controle de versões distribuído, usado principalmente no desenvolvimento de software',
+    image: require('../assets/images/git.jpg')
+  },
+  {
+    name: 'GitHub',
+    description: 'Plataforma de hospedagem de código-fonte e arquivos com controle de versão usando o Git',
+    image: require('../assets/images/github.jpg')
+  },
+  {
+    name: 'Notion',
+    description: 'Aplicação que fornece componentes tais como notas, bases de dados, quadros, wikis, calendários e lembretes',
+    image: require('../assets/images/notion.jpg')
+  },
+  {
+    name: 'ReactJS',
+    description: 'Biblioteca JavaScript de código aberto com foco em criar interfaces de usuário em páginas web',
+    image: require('../assets/images/reactjs.jpg')
+  },
+  {
+    name: 'Trello',
+    description: 'Aplicativo de gerenciamento de projeto baseado na web',
+    image: require('../assets/images/trello.jpg')
+  },
+  {
+    name: 'WhatsApp',
+    description: 'Aplicativo multiplataforma de mensagens instantâneas e chamadas de voz para smartphones',
+    image: require('../assets/images/whatsapp.jpg')
+  },
+  {
+    name: 'Zoom',
+    description: 'Programa de teleconferência de vídeo proprietário desenvolvido pela Zoom Video Communications',
+    image: require('../assets/images/zoom.jpg')
+  }
+];
+
+export default tools;
