98fd4367bf4feff5f8db1b4ea30635b4e7aaad59
Filipe Alves <filipe.alvesdsouza@gmail.com>
2022-03-08 16:05:51 -0300

    Implements cards pagination feature

--- src/App.js
+++ src/App.js
@@ -1,7 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Header from './components/Header';
+import Pagination from './components/Pagination';
+import ToolsList from './components/ToolsList';
+import tools from './static/tools';
 
 export default function App() {
+  const [firstToolIndex, setFirstToolIndex] = useState(0);
+  const [pageActionLabel, setPageActionLabel] = useState('next');
+
+  function goToNextPage() {
+    setFirstToolIndex(5);
+    setPageActionLabel('back');
+  }
+  
+  function backToPreviousPage() {
+    setFirstToolIndex(0);
+    setPageActionLabel('next');
+  }
+
   return (
-    <h1>Cards - ReactJS</h1>
+    <React.Fragment>
+      <Header />
+
+      <ToolsList
+        tools={tools}
+        firstToolIndex={firstToolIndex}
+      />
+
+      <Pagination
+        whereToGo={pageActionLabel}
+        action={pageActionLabel === 'next' ? goToNextPage : backToPreviousPage}
+      />
+    </React.Fragment>
   );
 }
